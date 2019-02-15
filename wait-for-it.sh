fc7a7eb20ec393689c0c795859101ea50343fa0e
chin8628 <boon.8628@gmail.com>
2019-02-15 17:51:10 +0700

    Added dependencies

---
+++ wait-for-it.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Database is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Database is up - executing command"
+exec $cmd
