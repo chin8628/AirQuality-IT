e1834c9117be274bbbdee90b0ad98f31a10f6c2a
chin8628 <boonyarith.pir@gmail.com>
2019-04-16 12:12:58 +0700

    Updated static url

--- airquality_itkmitl/settings.py
+++ airquality_itkmitl/settings.py
@@ -128,7 +128,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 STATIC_ROOT = '/code/static/'
 
 REST_FRAMEWORK = {
