87eeb98298b6068180ddc6e4b757b7235d5b06b3
chin8628 <boonyarith.pir@gmail.com>
2019-04-16 18:02:43 +0700

    Updated db field in views

--- external_api/views.py
+++ external_api/views.py
@@ -15,20 +15,20 @@ from .serializers import AirQualitySerializer, DeviceSerializer
 
 @require_POST
 def add(request):
-	token = request.POST.get('token')
-	device_id = request.POST.get('id')
+	token = request.POST.get('token', 0)
+	device_id = request.POST.get('id', 0)
 
 	try:
 		Device.objects.get(device_id=device_id, token=token)
 	except ObjectDoesNotExist:
 		return HttpResponse('Failed: Token or Device ID not found')
 
-	pm10 = request.POST.get('pm10', 0)
+	pm100 = request.POST.get('pm100', 0)
 	pm25 = request.POST.get('pm25', 0)
-	pm1 = request.POST.get('pm1', 0)
+	pm10 = request.POST.get('pm10', 0)
 
 	device = Device.objects.get(device_id=device_id)
-	AirQuality(device_id=device, pm1=pm1, pm10=pm10, pm25=pm25).save()
+	AirQuality(device_id=device, pm10=pm10, pm100=pm100, pm25=pm25).save()
 
 	return HttpResponse("Done")
 
